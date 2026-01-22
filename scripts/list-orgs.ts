0b18a848e09a6ad34e210e932b7116766f6ec67a
MarkyMo <mark@lissaman.com>
2026-01-22 16:19:26 +0000

    a

--- scripts/list-orgs.ts
+++
@@ -1,9 +0,0 @@
-
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  const orgs = await prisma.organization.findMany({ include: { members: { include: { user: true } } } });
-  console.log('Organizations:', JSON.stringify(orgs, null, 2));
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
-
