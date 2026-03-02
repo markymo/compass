f347526f921fd44edb741dfb6339f28160a9a0d1
MarkyMo <mark@lissaman.com>
2026-03-02 14:51:05 +0000

    chore: Clean up numerous debug and temporary files, and replace `src/middleware.ts` with `src/proxy.ts`.

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,14 +1,16 @@
 import NextAuth from "next-auth"
 import authConfig from "./auth.config"
 
-export const { auth: middleware } = NextAuth(authConfig)
+const { auth } = NextAuth(authConfig)
 
-export default middleware((req) => {
+export const proxy = auth((req) => {
     if (!req.auth && req.nextUrl.pathname !== "/login") {
         const newUrl = new URL("/login", req.nextUrl.origin)
         return Response.redirect(newUrl)
     }
 })
+
+export default proxy
 
 export const config = {
     matcher: ["/((?!api|_next/static|_next/image|favicon.ico|login|how-it-works|about|$).*)"],
