c0f8f000234a130aadd837a73d7e0875b1434394
MarkyMo <mark@lissaman.com>
2026-06-22 19:31:20 +0100

    refactor: remove engagementName field from test export payloads

--- test_db.ts
+++ test_db.ts
@@ -1,7 +1,7 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 const prisma = new PrismaClient();
 async function main() {
-  const q = await prisma.question.findFirst({ where: { answer: { not: null } } });
+  const q = await prisma.question.findFirst({ where: { answer: { not: Prisma.AnyNull } } });
   console.log("Answer JSON type:", typeof q?.answer, "Value:", q?.answer);
 }
 main().catch(console.error).finally(() => prisma.$disconnect());
