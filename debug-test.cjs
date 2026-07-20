00e738311d94e122952f352b45e755dfc4bfaa07
MarkyMo <mark@lissaman.com>
2026-07-20 01:34:25 +0100

    chore: remove temporary debug testing files

--- debug-test.cjs
+++
@@ -1,6 +0,0 @@
-const { JSDOM } = require('jsdom');
-const React = require('react');
-const { render } = require('@testing-library/react');
-const { DataSchemaTab } = require('./src/components/client/data-schema-tab');
-
-// But DataSchemaTab is in TypeScript and uses JSX. We cannot simply require it without babel/ts-node.
